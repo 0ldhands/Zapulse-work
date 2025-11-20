5deb220ce88ed0b963ce6f5a2e7c6a9cf7c5d474
0ldhands <imakevinp11@gmail.com>
2025-11-20 19:20:38 +0530

    lake

--- src/component/intro-component/BrandIntelligenceSection.jsx
+++ src/component/intro-component/BrandIntelligenceSection.jsx
@@ -102,7 +102,7 @@ const BrandIntelligenceSection = () => {
                 <>
                   {/* Image Area */}
                   <div className="w-full mb-4 rounded-lg overflow-hidden shadow-lg h-48 sm:h-64 md:h-72">
-                    {/* <img src={Brand} alt="Brand" /> */}
+                    <img src={Brand} alt="Brand" />
                                       </div>
 
                   {/* Text Description */}
