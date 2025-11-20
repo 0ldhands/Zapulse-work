59dd1b99da540423747f855d30ce5fa8d3d9b8bd
0ldhands <imakevinp11@gmail.com>
2025-11-20 17:51:18 +0530

    make

--- src/component/intro-component/BrandIntelligenceSection.jsx
+++ src/component/intro-component/BrandIntelligenceSection.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import Brand from "../../../assets/images/brand-intel-img-2.png"
-import vector1 from "../../../assets/images/vector/vector-1.png"
-import vector2 from "../../../assets/images/vector/vector-2.png"
+import Brand from "../../assets/images/brand-intel-img-2.png"
+import vector1 from "../../assets/images/vector/vector-1.png"
+import vector2 from "../../assets/images/vector/vector-2.png"
 
 // Define the content for each tab
 const tabs = [
